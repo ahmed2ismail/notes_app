7ff08541b53c419e5fda8d774b8f08a8c5fd3b69
ahmed2ismail <ah.hany5050@gmail.com>
2026-01-03 20:08:07 +0200

    create type adapter for note model

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
+void main()  {
   runApp(const NotesApp());
 }
 
--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,22 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+// اسم الملف اللي هيتولد فيه الكود تلقائياً
+part 'note_model.g.dart'; // .g => generated / .dart => Dart file
+// هنعمل هنا generate للـ Adapter الخاص بالـ Hive للـ NoteModel (generate type adapters)
+
+// @HiveType(typeId: 0) => ال annotation دي بتقول لـ Hive (build runner اللي هي ال) ان الكلاس ده هيبقى موديل هيخزن جواه بيانات يعني المكان دا قيه كود هيتعمله توليد تلقائياً
+@HiveType(typeId: 0) // كل موديل لازم ياخد ID فريد
+class NoteModel extends HiveObject {
+  @HiveField(0) // كل متغير بياخد رقم (Index) وبيكون تابع للكلاس
   final String title;
+
+  @HiveField(1)
   final String content;
+
+  @HiveField(2)
   final DateTime date;
+
+  @HiveField(3)
   final int color;
 
   NoteModel({
