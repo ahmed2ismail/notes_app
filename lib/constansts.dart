cce42faf1c6ceee1d548e99f5b381920128f8578
ahmed2ismail <ah.hany5050@gmail.com>
2026-01-03 20:20:43 +0200

    init hive and open notes box

--- lib/constansts.dart
+++ lib/constansts.dart
@@ -1,3 +1,4 @@
 import 'dart:ui';
 
 const kPrimaryColor =  Color(0xff62FCD7);
+const kNotesBoxName = 'notes_box';
--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,5 +1,6 @@
-import 'package:hive/hive.dart';
+// اول خطوة بعد اضافة مكتبة الـ Hive في الـ pubspec.yaml هي ان احنا نعرف الموديل اللي هنخزن جواه البيانات في الـ Hive
 
+import 'package:hive/hive.dart';
 // اسم الملف اللي هيتولد فيه الكود تلقائياً
 part 'note_model.g.dart'; // .g => generated / .dart => Dart file
 // هنعمل هنا generate للـ Adapter الخاص بالـ Hive للـ NoteModel (generate type adapters)
@@ -26,3 +27,12 @@ class NoteModel extends HiveObject {
     required this.color,
   });
 }
+
+// بعد ما نخلص كتابة الموديل نروح للترمينال ونكتب الامر ده عشان نعمل generate للـ Adapter
+// flutter packages pub run build_runner build
+// الامر هيولد ملف اسمه note_model.g.dart جوه نفس الفولدر اللي فيه الموديل
+// الملف هيبقى فيه كود جاهز للتعامل مع الـ Hive عشان يخزن ويقرأ البيانات من الصندوق (Box) بتاع الـ Hive
+// الـ Adapter هو وسيط بين الموديل وصندوق الـ Hive عشان يقدر يخزن ويقرأ البيانات بشكل صحيح
+// بعد كده هنروح لملف ال main.dart عشان نعمل initialize للـ Hive ونسجل الـ Adapter ونفتح الصندوق الخاص بالملاحظات جوه ال main function
+// لو عايزين نعمل تحديث للـ Adapter بعد ما نعدل في الموديل نستخدم الامر ده
+// flutter packages pub run build_runner build --delete-conflicting-outputs
